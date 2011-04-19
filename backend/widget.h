d9408793883c95cfef2b0fa2bf07ff4fe608dcc9
m.owoc.cyc@gmail.com <m.owoc.cyc@gmail.com@5480fba8-047d-5e17-b689-38145b62bb2d>
2011-04-19 21:17:33 +0000

    widget.h

---
+++ backend/widget.h
@@ -0,0 +1,51 @@
+#ifndef WIDGET_H
+#define WIDGET_H
+
+#include <QWidget>
+#include <qsqldatabase.h>
+#include <qsqlrecord.h>
+#include <qsqlquery.h>
+#include <qsqlerror.h>
+#include <QMessageBox>
+#include "Cardreader.h"
+#include <vector>
+#include <algorithm>
+#include <list>
+#include <QThread>
+
+
+namespace Ui {
+    class Widget;
+}
+
+class Widget : public QWidget
+{
+    Q_OBJECT
+
+public:
+    explicit Widget(QWidget *parent = 0);
+    ~Widget();
+    void setup();
+    void polacz(QSqlDatabase baza);
+    void konsultacje(QString nazwisko);
+    void pobierz();
+    void rozlacz(QSqlDatabase baza);
+
+    void ustaw_pokoj(int nr);
+    void wyniki();
+    void kolejka();
+    void test();
+    std::list<int> sheduler(int priorytety[], int n);
+private:
+    Ui::Widget *ui;
+    CardReader karta;
+
+
+public slots:
+
+    void ogloszenia();
+    void readCard();
+    //void on_tableView_activated(QModelIndex index);
+};
+
+#endif // WIDGET_H
