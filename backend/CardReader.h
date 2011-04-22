91e5804e13d93172ba53e35e5d89602c844558b7
glapa389@gmail.com <glapa389@gmail.com@5480fba8-047d-5e17-b689-38145b62bb2d>
2011-04-22 13:52:52 +0000

    ...

--- backend/CardReader.h
+++ backend/CardReader.h
@@ -1,7 +1,13 @@
 #ifndef CARDREADER_H
 #define CARDREADER_H
-#include <wintypes.h>
+#pragma once
+#include <wintypes.h> 
 #include <winscard.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string>
+
+using namespace std;
 
 class CardReader
 {
@@ -9,20 +15,24 @@ class CardReader
         CardReader();
         ~CardReader();
         unsigned int getIndex(void);
-        char* getImie(void);
-        char* getNazwisko(void);
+        string getImie(void);
+        string getNazwisko(void);
         bool isCard(void);
-    protected:
     private:
         SCARDHANDLE hCard;
         SCARDCONTEXT hContext;
         SCARD_IO_REQUEST sRecvPci;
         SCARD_READERSTATE rgReaderStates;
-        BYTE pbRecvBuffer[257];
+		BYTE pbRecvBuffer[257];
         DWORD dwRecvLength, dwPref, dwReaders;
         LPSTR mszReaders;
-        LPCSTR mszGroups;
+        LPCSTR mszGroups; 
         long rv;
+		bool is_present;
+		string imie;
+		string nazwisko;
+		unsigned int indeks;
+		void getPersonalData(void);
 };
 
 #endif // CARDREADER_H
