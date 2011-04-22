51b44fb3194db1b59701d51ccf0e2e0d498f9265
glapa389@gmail.com <glapa389@gmail.com@5480fba8-047d-5e17-b689-38145b62bb2d>
2011-04-22 13:50:49 +0000

    pełna obsługa buforowania danych. Zwraca imię, nazwisko oraz nr indeksu.

--- backend/CardReader.cpp
+++ backend/CardReader.cpp
@@ -1,27 +1,26 @@
 #include "CardReader.h"
-#include <stdlib.h>
-#include <stdio.h>
-#include <string>
-
-using namespace std;
 
 CardReader::CardReader()
 {
 	try
 	{
+		is_present=false;
+		indeks=0;
+		imie="brak";
+		nazwisko="brak";
 		mszReaders=0;
 		dwReaders=SCARD_AUTOALLOCATE;
 		dwRecvLength=sizeof(pbRecvBuffer);
 		rv=SCardEstablishContext(SCARD_SCOPE_USER, 0, 0, &hContext);
 		if(rv!=SCARD_S_SUCCESS)
 		{
-			printf("SCardEstablishContext ");
+			printf("SCardEstablishContext "); 
 			throw rv;
 		}
-		rv=SCardListReaders(hContext, 0, (LPSTR)&mszReaders, &dwReaders);
+		rv=SCardListReaders(hContext, 0, (LPWSTR)&mszReaders, &dwReaders);
 		if(rv!=SCARD_S_SUCCESS)
 		{
-			printf("SCardListReaders ");
+			printf("SCardListReaders "); 
 			throw rv;
 		}
 		rgReaderStates.szReader=mszReaders;
@@ -38,33 +37,41 @@ CardReader::~CardReader()
 		SCardFreeMemory(hContext, mszReaders);
 	SCardReleaseContext(hContext);
 }
-char* CardReader::getImie(void)
+string CardReader::getImie(void)
 {
-	return "Lukasz";
+	if(!is_present)
+		getPersonalData();
+	return imie;
 }
-char* CardReader::getNazwisko(void)
+string CardReader::getNazwisko(void)
 {
-	return "Glapinski";
+	if(!is_present)
+		getPersonalData();
+	return nazwisko;
+}
+unsigned int CardReader::getIndex(void)
+{
+	if(!is_present)
+		getPersonalData();
+	return indeks;
 }
 bool CardReader::isCard(void)
 {
 	SCardGetStatusChange(hContext, 1000, &rgReaderStates, 1);
 	if(rgReaderStates.dwEventState & SCARD_STATE_PRESENT)
 		return true;
+	else
+		is_present=false;
 	return false;
 }
-unsigned int CardReader::getIndex(void)
+void CardReader::getPersonalData(void)
 {
-	if (!isCard() )
-		return 0;
 	BYTE select_mf_cmd[]={0x00, 0xA4, 0x00, 0x0C, 0x02, 0x3F, 0x00};
 	BYTE select_df_cmd[]={0x00, 0xA4, 0x04, 0x00, 0x07, 0xD6, 0x16, 0x00, 0x00, 0x30, 0x01, 0x01};
 	BYTE select_ef_cmd[]={0x00, 0xA4, 0x00, 0x0C, 0x02, 0x00, 0x02};
 	BYTE read_cmd[]={0x00, 0xB0, 0x00, 0x00, 0xFF};
 	string result[50];  // parsuje rekordy do stringa
-	int index=0;
-	bool last_byte_control=false, hex_0x0C=false;
-	unsigned int indeks=0;
+	int j=0;
 	try
 	{
 		rv=SCardConnect(hContext, rgReaderStates.szReader, SCARD_SHARE_EXCLUSIVE, SCARD_PROTOCOL_T0, &hCard, &dwPref);
@@ -73,7 +80,7 @@ unsigned int CardReader::getIndex(void)
 			printf("SCardConnect ");
 			throw rv;
 		}
-		rv=SCardTransmit(hCard, SCARD_PCI_T0, select_mf_cmd, sizeof(select_mf_cmd), &sRecvPci, pbRecvBuffer, &dwRecvLength);
+		rv=SCardTransmit(hCard, SCARD_PCI_T0, select_mf_cmd, sizeof(select_mf_cmd), 0, pbRecvBuffer, &dwRecvLength);	
 		if(rv!=SCARD_S_SUCCESS)
 		{
 			SCardEndTransaction(hCard, SCARD_LEAVE_CARD);
@@ -82,7 +89,7 @@ unsigned int CardReader::getIndex(void)
 			throw rv;
 		}
 		dwRecvLength=sizeof(pbRecvBuffer);
-		rv=SCardTransmit(hCard, SCARD_PCI_T0, select_df_cmd, sizeof(select_df_cmd), &sRecvPci, pbRecvBuffer, &dwRecvLength);
+		rv=SCardTransmit(hCard, SCARD_PCI_T0, select_df_cmd, sizeof(select_df_cmd), 0, pbRecvBuffer, &dwRecvLength);	
 		if(rv!=SCARD_S_SUCCESS)
 		{
 			SCardEndTransaction(hCard, SCARD_LEAVE_CARD);
@@ -91,7 +98,7 @@ unsigned int CardReader::getIndex(void)
 			throw rv;
 		}
 		dwRecvLength=sizeof(pbRecvBuffer);
-		rv=SCardTransmit(hCard, SCARD_PCI_T0, select_ef_cmd, sizeof(select_ef_cmd), &sRecvPci, pbRecvBuffer, &dwRecvLength);
+		rv=SCardTransmit(hCard, SCARD_PCI_T0, select_ef_cmd, sizeof(select_ef_cmd), 0, pbRecvBuffer, &dwRecvLength);	
 		if(rv!=SCARD_S_SUCCESS)
 		{
 			SCardEndTransaction(hCard, SCARD_LEAVE_CARD);
@@ -100,7 +107,7 @@ unsigned int CardReader::getIndex(void)
 			throw rv;
 		}
 		dwRecvLength=sizeof(pbRecvBuffer);
-		rv=SCardTransmit(hCard, SCARD_PCI_T0, read_cmd, sizeof(read_cmd), &sRecvPci, pbRecvBuffer, &dwRecvLength);
+		rv=SCardTransmit(hCard, SCARD_PCI_T0, read_cmd, sizeof(read_cmd), 0, pbRecvBuffer, &dwRecvLength);	
 		if(rv!=SCARD_S_SUCCESS)
 		{
 			SCardEndTransaction(hCard, SCARD_LEAVE_CARD);
@@ -108,6 +115,7 @@ unsigned int CardReader::getIndex(void)
 			printf("SCardTransmit ");
 			throw rv;
 		}
+		bool last_byte_control=false, hex_0x0C=false;
 		for(unsigned int i=0;i<dwRecvLength-2;i++)
 		{
 			int code=pbRecvBuffer[i];
@@ -118,7 +126,7 @@ unsigned int CardReader::getIndex(void)
 				{
 					if(hex_0x0C==false)
 					{
-						result[index]+=c;
+						result[j]+=c;
 					}
 				}
 				if(hex_0x0C==true)
@@ -127,40 +135,22 @@ unsigned int CardReader::getIndex(void)
 				}
 				if(((code==0x0C) || (code==0x13) || (code==0x18)) && (last_byte_control==false))
 				{
-					last_byte_control=true; // zeby nie traktowal dlugosci danych w rekordzie jako bajtu kontrolnego
-					index++;
+					last_byte_control=true;
+					j++;
 					hex_0x0C=true;
 				}
 				else
 					last_byte_control=false;
 			}
 		}
-		/*string dane[9];
-		int nr_danych=0;
-		for(int i=1;i<7;i++)
-		{
-		if(!result[i].empty())
-		{
-		if(i>0 && nr_danych<9)
-		{
-		if(i!=5)
-		{
-		dane[nr_danych]=result[i];
-		nr_danych++;
-		}
-		else
-		{
-		dane[nr_danych-1]+=" "+result[i];
-		}
-		}
-		}
-		}*/
 		SCardDisconnect(hCard, SCARD_UNPOWER_CARD);
+		is_present=true;
+		imie=result[4];
+		nazwisko=result[3].substr(0,result[3].length()-1);
 		indeks=atoi(result[6].c_str());
 	}
 	catch(long e)
 	{
 		printf("error no. %lX\n",e);
 	}
-	return indeks;
 }
