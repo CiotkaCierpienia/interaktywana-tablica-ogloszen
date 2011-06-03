6d691ef33efd28e8b33008973911150eb06f7b4d
glapa389@gmail.com <glapa389@gmail.com@5480fba8-047d-5e17-b689-38145b62bb2d>
2011-06-03 13:43:30 +0000

    dodane komentarze i tłumaczenie błędów na tekst

--- backend/CardReader.cpp
+++ backend/CardReader.cpp
@@ -4,43 +4,43 @@ CardReader::CardReader()
 {
         try
         {
-                is_present=false;
-                indeks=0;
+                is_present=false;   //karta wyciągnięta
+                indeks=0;           //czyścimy bufor
                 imie="brak";
                 nazwisko="brak";
                 mszReaders=0;
-                dwReaders=SCARD_AUTOALLOCATE;
-                dwRecvLength=sizeof(pbRecvBuffer);
-                rv=SCardEstablishContext(SCARD_SCOPE_USER, 0, 0, &hContext);
-                if(rv!=SCARD_S_SUCCESS)
+                dwReaders=SCARD_AUTOALLOCATE;       //automatyczna alokacja pamięci dla danych czytnika
+                dwRecvLength=sizeof(pbRecvBuffer);  //długość danych przychodzących
+                rv=SCardEstablishContext(SCARD_SCOPE_USER, 0, 0, &hContext);    //ustawiamy kontekst
+                if(rv!=SCARD_S_SUCCESS)             //jeżeli operacja się nie powiodła wyrzuca wyjątek
                 {
                         printf("SCardEstablishContext ");
                         throw rv;
                 }
-                rv=SCardListReaders(hContext, 0, (LPSTR)&mszReaders, &dwReaders);
+                rv=SCardListReaders(hContext, 0, (LPSTR)&mszReaders, &dwReaders);   //listuje podłączone czytniki
                 if(rv!=SCARD_S_SUCCESS)
                 {
                         printf("SCardListReaders ");
                         throw rv;
                 }
-                rgReaderStates.szReader=mszReaders;
-                rgReaderStates.dwCurrentState=SCARD_STATE_EMPTY;
+                rgReaderStates.szReader=mszReaders;                 //ustawia strukturę stanów czytnika
+                rgReaderStates.dwCurrentState=SCARD_STATE_EMPTY;    //status na pusty
         }
         catch (long e)
         {
-                printf("error no. %lX\n",e);
+                printf("error: %s\n",pcsc_stringify_error(e));
         }
 }
 CardReader::~CardReader()
 {
-        if(mszReaders!=NULL)
+        if(mszReaders!=NULL)                            //zwalnia zaalokowaną pamięć
                 SCardFreeMemory(hContext, mszReaders);
-        SCardReleaseContext(hContext);
+        SCardReleaseContext(hContext);                  //zwalnia kontekst
 }
 string CardReader::getImie(void)
 {
-        if(!is_present)
-                getPersonalData();
+        if(!is_present)             //jeśli karta była wyciągnięta
+                getPersonalData();  //pobiera dane do bufora
         return imie;
 }
 string CardReader::getNazwisko(void)
@@ -57,39 +57,39 @@ unsigned int CardReader::getIndex(void)
 }
 bool CardReader::isCard(void)
 {
-        SCardGetStatusChange(hContext, 1000, &rgReaderStates, 1);
-        if(rgReaderStates.dwEventState & SCARD_STATE_PRESENT)
+        SCardGetStatusChange(hContext, 1000, &rgReaderStates, 1);   //wykrywa zmianę stanu czytnika
+        if(rgReaderStates.dwEventState & SCARD_STATE_PRESENT)       //jeżeli karta jest w czytniku
                 return true;
         else
-                is_present=false;
+                is_present=false;                                   //karty nie ma w czytniku
         return false;
 }
 void CardReader::getPersonalData(void)
 {
+        //komendy wybrania aplikacji i pobrania danych
         BYTE select_mf_cmd[]={0x00, 0xA4, 0x00, 0x0C, 0x02, 0x3F, 0x00};
         BYTE select_df_cmd[]={0x00, 0xA4, 0x04, 0x00, 0x07, 0xD6, 0x16, 0x00, 0x00, 0x30, 0x01, 0x01};
         BYTE select_ef_cmd[]={0x00, 0xA4, 0x00, 0x0C, 0x02, 0x00, 0x02};
         BYTE read_cmd[]={0x00, 0xB0, 0x00, 0x00, 0xFF};
-        string result[50];  // parsuje rekordy do stringa
         int j=0;
         try
         {
-                rv=SCardConnect(hContext, rgReaderStates.szReader, SCARD_SHARE_EXCLUSIVE, SCARD_PROTOCOL_T0, &hCard, &dwPref);
+                rv=SCardConnect(hContext, rgReaderStates.szReader, SCARD_SHARE_EXCLUSIVE, SCARD_PROTOCOL_T0, &hCard, &dwPref);  //łączy z kartą
                 if(rv!=SCARD_S_SUCCESS)
                 {
                         printf("SCardConnect ");
-                        is_present=false;
-                        rgReaderStates.dwEventState&=SCARD_STATE_EMPTY;
-                        indeks=0;
+                        is_present=false;                               //karty nie ma
+                        rgReaderStates.dwEventState&=SCARD_STATE_EMPTY; //status na pusty
+                        indeks=0;                                       //czyszczenie buforów
                         imie="";
                         nazwisko="";
                         throw rv;
                 }
                 dwRecvLength=sizeof(pbRecvBuffer);
-                rv=SCardTransmit(hCard, SCARD_PCI_T0, select_mf_cmd, sizeof(select_mf_cmd), 0, pbRecvBuffer, &dwRecvLength);
+                rv=SCardTransmit(hCard, SCARD_PCI_T0, select_mf_cmd, sizeof(select_mf_cmd), 0, pbRecvBuffer, &dwRecvLength);    //przesłanie komendy
                 if(rv!=SCARD_S_SUCCESS)
                 {
-                        SCardDisconnect(hCard, SCARD_UNPOWER_CARD);
+                        SCardDisconnect(hCard, SCARD_UNPOWER_CARD); //rozłączamy z kartą
                         is_present=false;
                         rgReaderStates.dwEventState&=SCARD_STATE_EMPTY;
                         indeks=0;
@@ -137,8 +137,9 @@ void CardReader::getPersonalData(void)
                         printf("SCardTransmit ");
                         throw rv;
                 }
-                bool last_byte_control=false, hex_0x0C=false;
-                for(unsigned int i=0;i<dwRecvLength-2;i++)
+                string result[50];                              //przechowuje sformatowany wynik
+                bool last_byte_control=false, hex_0x0C=false;   //zmienne kontrolne
+                for(unsigned int i=0;i<dwRecvLength-2;i++)      //parsuje odebrane dane do stringa
                 {
                         int code=pbRecvBuffer[i];
                         if(code>0)
@@ -150,6 +151,7 @@ void CardReader::getPersonalData(void)
                                         {
                                                 result[j]+=c;
                                         }
+                                }
                                 }
                                 if(hex_0x0C==true)
                                 {
@@ -166,13 +168,13 @@ void CardReader::getPersonalData(void)
                         }
                 }
                 SCardDisconnect(hCard, SCARD_UNPOWER_CARD);
-                is_present=true;
-                imie=result[4];
+                is_present=true;    //karta w czytniku
+                imie=result[4];     //ustawianie bufora
                 nazwisko=result[3].substr(0,result[3].length()-1);
                 indeks=atoi(result[6].c_str());
         }
         catch(long e)
         {
-                printf("error no. %lX\n",e);
+                printf("error: %s\n",pcsc_stringify_error(e));
         }
 }
